574b47a2bcb215455356e006259135e87f896834
darshanrw <Dwadekar4575@conestogac.on.ca>
2024-12-01 20:06:42 -0500

    removed all files

--- Hello_world/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    const name = req.query.name || "world";
-    context.res = { body: `Hello, ${name}!` };
-};
